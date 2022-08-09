73425984c140e5cac8d63a4878bb409b8aa99f3e
sDos280 <107134807+sDos280@users.noreply.github.com>
2022-08-09 08:49:25 +0300

    adding files

---
+++ main.c
@@ -0,0 +1,149 @@
+#include "raylib.h"
+#include <stdio.h>
+#include "open-simplex-noise.c"
+
+double remap(double x, double in_min, double in_max, double out_min, double out_max);
+int getTileCase(int topleft, int topright, int buttomleft, int buttomright);
+
+int main(void)
+{
+    const int screenWidth = 800;
+    const int screenHeight = 800;
+
+    const int rows = 400;
+    const int columns = 400;
+
+    const float tile_x = (float)screenWidth / (columns - 1);
+    const float tile_y = (float)screenHeight / (rows - 1);
+
+    double grid[columns][rows];
+
+    double offset  = 0;
+
+    struct osn_context *ctx;
+    open_simplex_noise(GetRandomValue(1, 100), &ctx);
+
+    InitWindow(screenWidth, screenHeight, "raylib [other] example - marching squres algorithm");
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        offset += 0.01f;
+
+        for (int r = 0; r < rows; r++)
+        {
+            for (int c = 0; c < columns; c++)
+            {
+                grid[c][r] = remap(open_simplex_noise3(ctx, c*0.03, r*0.03, offset), -1, 1, 0, 1); 
+            }
+        }
+
+        BeginDrawing();
+
+            ClearBackground(BLACK);
+
+            for (int r = 0; r < rows - 1; r++)
+            {
+                for (int c = 0; c < columns - 1; c++)
+                {
+                    int Case = getTileCase(grid[c][r] >= 0.5 ? 1 : 0, grid[c + 1][r] >= 0.5 ? 1 : 0, grid[c][r + 1] >= 0.5 ? 1 : 0, grid[c + 1][r + 1] >= 0.5 ? 1 : 0);
+
+                    Vector2 topleftPoint = (Vector2){c * tile_x, r * tile_y};
+                    Vector2 toprightPoint = (Vector2){(c + 1) * tile_x, r * tile_y};
+                    Vector2 buttomleftPoint = (Vector2){c * tile_x, (r + 1) * tile_y};
+                    Vector2 buttomrightPoint = (Vector2){(c + 1) * tile_x, (r + 1) * tile_y};
+
+                    Vector2 middleUpPoint = (Vector2){(topleftPoint.x + toprightPoint.x) / 2.0, (topleftPoint.y + toprightPoint.y) / 2.0};
+                    Vector2 middleDownPoint = (Vector2){(buttomleftPoint.x + buttomrightPoint.x) / 2.0, (buttomleftPoint.y + buttomrightPoint.y) / 2.0};
+                    Vector2 middleRightPoint = (Vector2){(toprightPoint.x  + buttomrightPoint.x) / 2.0, (toprightPoint.y + buttomrightPoint.y) / 2.0};
+                    Vector2 middleLeftPoint = (Vector2){(topleftPoint.x + buttomleftPoint.x) / 2.0, (topleftPoint.y + buttomleftPoint.y) / 2.0};
+
+                    switch (Case)
+                    {
+                        case 0:
+                            break;
+
+                        case 1:
+                            DrawLineV(middleDownPoint, middleLeftPoint, WHITE);
+                            break;
+
+                        case 2:
+                            DrawLineV(middleDownPoint, middleRightPoint, WHITE);
+                            break;
+
+                        case 3:
+                            DrawLineV(middleLeftPoint, middleRightPoint, WHITE);
+                            break;
+
+                        case 4:
+                            DrawLineV(middleUpPoint, middleRightPoint, WHITE);
+                            break;
+
+                        case 5:
+                            DrawLineV(middleUpPoint, middleLeftPoint, WHITE);
+                            DrawLineV(middleDownPoint, middleRightPoint, WHITE);
+                            break;
+
+                        case 6:
+                            DrawLineV(middleUpPoint, middleDownPoint, WHITE);
+                            break;
+
+                        case 7:
+                            DrawLineV(middleUpPoint, middleLeftPoint, WHITE);
+                            break;
+
+                        case 8:
+                            DrawLineV(middleUpPoint, middleLeftPoint, WHITE);
+                            break;
+
+                        case 9:
+                            DrawLineV(middleUpPoint, middleDownPoint, WHITE);
+                            break;
+
+                        case 10:
+                            DrawLineV(middleUpPoint, middleRightPoint, WHITE);
+                            DrawLineV(middleDownPoint, middleLeftPoint, WHITE);
+                            break;
+
+                        case 11:
+                            DrawLineV(middleUpPoint, middleRightPoint, WHITE);
+                            break;
+
+                        case 12:
+                            DrawLineV(middleLeftPoint, middleRightPoint, WHITE);
+                            break;
+
+                        case 13:
+                            DrawLineV(middleDownPoint, middleRightPoint, WHITE);
+                            break;
+
+                        case 14:
+                            DrawLineV(middleDownPoint, middleLeftPoint, WHITE);
+                            break;
+
+                        case 15:
+                            break;
+                    }
+                }
+            }
+        DrawFPS(10, 10);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    
+    open_simplex_noise_free(ctx);
+
+    return 0;
+}
+
+int getTileCase(int topleft, int topright, int buttomleft, int buttomright)
+{
+    return topleft * 8 + topright * 4 + buttomright * 2 + buttomleft;
+}
+
+double remap(double x, double in_min, double in_max, double out_min, double out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
---
+++ run.ps1
@@ -0,0 +1,2 @@
+gcc main.c -o main.exe -I include -L lib -lraylib -lopengl32 -lgdi32 -lwinmm
+start main.exe
