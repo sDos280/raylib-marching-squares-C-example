563a467908f1c89486941b29cd2904ad9ee621e3
Dor Shapira <107134807+sDos280@users.noreply.github.com>
2022-08-24 16:39:38 +0300

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,2 +1,2 @@
-gcc main.c "open-simplex-noise.c" -o main -I include -L lib -lraylib -lopengl32 -lgdi32 -lwinmm
+gcc main.c "include/open-simplex-noise.c" -o main -I include -L lib -lraylib -lopengl32 -lgdi32 -lwinmm
 start main.exe
